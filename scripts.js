77d134c4abda23079b566f7116a1ab0638f5a77f
Khang Lieu <khangdlieu@gmail.com>
2018-10-11 14:27:37 -0400

    Fixed errors

--- scripts.js
+++ scripts.js
@@ -2,27 +2,31 @@ var spawn = require("child_process").spawn;
 // processRPM = spawn("python3", ["-u", "dataReader.py"], {
 //   shell: true
 // });
-processSOC = spawn("./CanInterface", [], {
+// processSOC = spawn("./CanInterface", [], {
+//   shell: true
+// });
+
+processSOC = spawn("./canInterface", [], {
   shell: true
 });
-processRPM.unref();
 var rpm = document.getElementById("rpm");
 var soc = document.getElementById("soc");
 
 var b1 = document.querySelector(".ldBar");
 var b = new ldBar(b1);
 
-processRPM.stdout.setEncoding("utf8");
-processRPM.stdout.on("data", data => {
-  var str = data.toString();
-  var buf = str.split(":");
-  if (buf[0] == "rpm") {
-    rpm.textContent = "RPM: " + buf[1];
-  }
-});
+// processRPM.stdout.setEncoding("utf8");
+// processRPM.stdout.on("data", data => {
+//   var str = data.toString();
+//   var buf = str.split(":");
+//   if (buf[0] == "rpm") {
+//     rpm.textContent = "RPM: " + buf[1];
+//   }
+// });
 processSOC.stdout.on("data", data => {
   var str = data.toString();
   var buf = str.split(":");
+  console.log(str);
   if (buf[0] == "soc") {
     b.set(parseInt(buf[1]));
   } else if (buf[0] == "rpm") {
