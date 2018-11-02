5f5c95eb800ab2398147de79f7f93c87959e0e50
Mathew Verghese <mathew6@vt.edu>
2018-11-02 10:33:59 -0400

    add global to avoid storing nonexistent buffer value

--- dataReader.py
+++ dataReader.py
@@ -5,6 +5,6 @@ y = 0
 while(1):
     print("rpm:", str(x))
     print("soc:", str(y))
-    time.sleep(0.001)
+    time.sleep(.001)
     x = x + 1
     y = y + 0.001
--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ which outputs CAN data
 /* Spawns an instance of dataReader.py, which writes to stdout 
 test rpm and soc data 
 --------Used for testing-------*/
-processInputs = spawn("python3", ["-u", "dataReader.py"], {
+processInputs = spawn("python", ["-u", "dataReader.py"], {
   shell: true
 });
 
@@ -21,6 +21,7 @@ var rpm = document.getElementById("rpm");
 var b1 = document.querySelector(".ldBar");
 var b = new ldBar(b1);
 
+let curr_rpm = 0;
 //Reads in stdout, processes data to display on screen.
 processInputs.stdout.on("data", data => {
   var str = data.toString();
@@ -29,6 +30,10 @@ processInputs.stdout.on("data", data => {
     b.set(parseInt(buf[1]));
   } else if (buf[0] == "rpm") {
     let newbuf = buf[1].split("\n");
-    rpm.textContent = "RPM: " + newbuf[0];
+    if (newbuf[0].trim().length != 0) {
+      curr_rpm = newbuf[0];
+    }
+    console.log(curr_rpm);
+    rpm.textContent = "RPM: " + curr_rpm;
   }
 });
