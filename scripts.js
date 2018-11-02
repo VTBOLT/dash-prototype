e972e12b2c8a65f18020c856d97c9da831224168
Mathew Verghese <mathew6@vt.edu>
2018-11-02 14:38:04 -0400

    fix soc flashing on and off error

--- scripts.js
+++ scripts.js
@@ -21,19 +21,22 @@ var rpm = document.getElementById("rpm");
 var b1 = document.querySelector(".ldBar");
 var b = new ldBar(b1);
 
+let curr_soc = 0;
 let curr_rpm = 0;
 //Reads in stdout, processes data to display on screen.
 processInputs.stdout.on("data", data => {
   var str = data.toString();
   let buf = str.split(":");
   if (buf[0] == "soc") {
-    b.set(parseInt(buf[1]));
+    if (buf[1].trim().length != 0) {
+      curr_soc = buf[1];
+    }
+    b.set(parseInt(curr_soc));
   } else if (buf[0] == "rpm") {
     let newbuf = buf[1].split("\n");
     if (newbuf[0].trim().length != 0) {
       curr_rpm = newbuf[0];
     }
-    console.log(curr_rpm);
     rpm.textContent = "RPM: " + curr_rpm;
   }
 });
