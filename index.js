81422f121b7c5a8a1424cecfb79777a4c1a1f08d
Quinton <qemiller@vt.edu>
2019-02-08 15:55:50 -0500

    removed extra lines

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function createWindow() {
     // Create the browser window 
     // (height and width of NHD-5.0-HDMI-N-RTXL-RTU)
     win = new BrowserWindow({ width: 800, height: 480 });
-
     // and load the index.html of the app.
     win.loadFile("index.html");
 }
