6b737a9507b5b8d268c314f72b358051f4fcb859
jcsmeth <jcsmeth@vt.edu>
2019-03-21 22:58:35 -0400

    stop menu from autohide

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const options = {message:'button working!'};
 function createWindow() {
     // Create the browser window 
     // (height and width of NHD-5.0-HDMI-N-RTXL-RTU)
-    win = new BrowserWindow({ width: 800, height: 480 });
+    win = new BrowserWindow({ 
+        width: 800, 
+        height: 480, 
+        autoHideMenuBar: false });
     // set full screen (matters for the pi)
     // COMMENT THIS OUT IF RUNNING ON YOUR OWN MACHINE
     // OR USE ESCAPE TO TOGGLE BACK TO WINDOWED
