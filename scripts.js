c0d8a2e301dceeade485653ac3754f2bc354baaa
jcsmeth <jcsmeth@vt.edu>
2019-05-02 12:30:04 -0400

    debug dc bus v substring

--- scripts.js
+++ scripts.js
@@ -396,7 +396,7 @@ function can_test() {
       case dcBusVAddr:
         // dc bus voltage calculation: bytes 0 and 1, 0.1 scale factor
         curr_dcbusv = (msg["data"][1] << 8 + msg["data"][0]) * 0.1;
-        debugdcbus.textContent = curr_dcbusv.toString().substring(0,3);
+        debugdcbus.textContent = curr_dcbusv.toString().substring(0,6);
 
         break;
       case mcTempsAddr:
