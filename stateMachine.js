9706b1651a6b09b4e86dcaea195258c1b168db92
nathanlm511 <nathanmoeliono@gmail.com>
2019-05-05 14:31:26 -0400

    implemeted state machine code

--- stateMachine.js
+++ stateMachine.js
@@ -31,32 +31,34 @@ function nonrace_states(event) {
 }
 
 function nonrace_statesGPIO() {
-    var message = "Waiting on accessory..."; 
-    var rpio = require('rpio');
-    //BMS Discharge Enable (Start Button)
-    rpio.open(29, rpio.INPUT);
-    //Pump Indicator
-    rpio.open(31, rpio.INPUT);
-    //ACC Switch
-    rpio.open(33, rpio.INPUT);
-    //IGN Switch
-    rpio.open(35, rpio.INPUT);
-    //Isolation Fault Signal
-    rpio.open(37, rpio.INPUT);
-    if (rpio.read(29)) {
-        document.location.replace("index.html"); 
-    } 
-    else if (rpio.read(37)) {
-        message = "ISOLATION FAULT DETECTED!";
+    while(true) {
+        var message = "Waiting on accessory..."; 
+        var rpio = require('rpio');
+        //BMS Discharge Enable (Start Button)
+        rpio.open(29, rpio.INPUT);
+        //Pump Indicator
+        rpio.open(31, rpio.INPUT);
+        //ACC Switch
+        rpio.open(33, rpio.INPUT);
+        //IGN Switch
+        rpio.open(35, rpio.INPUT);
+        //Isolation Fault Signal
+        rpio.open(37, rpio.INPUT);
+        if (rpio.read(29)) {
+            document.location.replace("index.html"); 
+        } 
+        else if (rpio.read(37)) {
+            message = "ISOLATION FAULT DETECTED!";
+        }
+        else if (rpio.read(35)) {
+            message = "Press the start button!"; 
+        }
+        else if (rpio.read(31)) {
+            message = "Waiting on ignition...";
+        }
+        else if (rpio.read(33)) {
+            message ="Pump is on!";
+        }    
+        document.getElementById("startUpText").innerHTML = message;
     }
-    else if (rpio.read(35)) {
-        message = "Press the start button!"; 
-    }
-    else if (rpio.read(31)) {
-        message = "Waiting on ignition...";
-    }
-    else if (rpio.read(33)) {
-        message ="Pump is on!";
-    }    
-    document.getElementById("startUpText").innerHTML = message;
 }
