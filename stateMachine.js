010d72c4bbb8ff7c9fd4efd1bb9fbddb49c11e3b
nathanlm511 <nathanmoeliono@gmail.com>
2019-05-11 13:34:33 -0400

    added listeners

--- stateMachine.js
+++ stateMachine.js
@@ -30,35 +30,70 @@ function nonrace_states(event) {
     document.getElementById("startUpText").innerHTML = message;
 }
 
-function nonrace_statesGPIO() {
-    while(true) {
-        var message = "Waiting on accessory..."; 
-        var rpio = require('rpio');
-        //BMS Discharge Enable (Start Button)
-        rpio.open(29, rpio.INPUT);
-        //Pump Indicator
-        rpio.open(31, rpio.INPUT);
-        //ACC Switch
-        rpio.open(33, rpio.INPUT);
-        //IGN Switch
-        rpio.open(35, rpio.INPUT);
-        //Isolation Fault Signal
-        rpio.open(37, rpio.INPUT);
-        if (rpio.read(29)) {
-            document.location.replace("index.html"); 
-        } 
-        else if (rpio.read(37)) {
-            message = "ISOLATION FAULT DETECTED!";
-        }
-        else if (rpio.read(35)) {
-            message = "Press the start button!"; 
-        }
-        else if (rpio.read(31)) {
-            message = "Waiting on ignition...";
-        }
-        else if (rpio.read(33)) {
-            message ="Pump is on!";
-        }    
-        document.getElementById("startUpText").innerHTML = message;
-    }
+function nonrace_statesGPIO() { 
+    var rpio = require('rpio');
+    //BMS Discharge Enable 
+    rpio.open(29, rpio.INPUT, rpio.PULL_UP);
+    //Pump Indicator
+    rpio.open(31, rpio.INPUT, rpio.PULL_UP);
+    //ACC Switch
+    rpio.open(33, rpio.INPUT, rpio.PULL_UP);
+    //IGN Switch
+    rpio.open(35, rpio.INPUT, rpio.PULL_UP);
+    //Isolation Fault Signal
+    rpio.open(37, rpio.INPUT, rpio.PULL_UP);
+    //poll for pin state change
+    rpio.poll(29, pollDischarge, rpio.POLL_DOWN);
+    rpio.poll(31, pollPump, rpio.POLL_DOWN);
+    rpio.poll(33, pollACC, rpio.POLL_DOWN);
+    rpio.poll(35, pollIGN, rpio.POLL_DOWN);
+    rpio.poll(37, pollISO, rpio.POLL_DOWN);  
 }
+
+function pollDischarge(pin) {
+    document.getElementById("startUpText").innerHTML = "BMS discharging";
+}
+
+function pollPump(pin) {
+    document.getElementById("startUpText").innerHTML = "Pumping";
+}
+
+function pollACC(pin) {
+    document.getElementById("startUpText").innerHTML = "Waiting on ignition...";
+}
+
+function pollIGN(pin) {
+    document.getElementById("startUpText").innerHTML = "Press the start button.";
+}
+
+function pollISO(pin) {
+    document.getElementById("startUpText").innerHTML = "ISOLATION FAULT!";
+}
+
+//only poll 29 and 37 in code
+/*
+//BMS must be discharging before any other buttons work
+function pollDischarge(pin) {}
+    rpio.poll(31, pollPump, rpio.POLL_DOWN);
+    rpio.poll(pin, null);
+}
+
+//Pump must be active in order to move on to ignition
+function pollPump(pin) {
+    rpio.poll(33, rpio.PULL_DOWN);
+    rpio.poll(pin, null);
+}
+
+//Accessory on + pump means the bike is ready for ignition
+function pollACC(pin) {
+    document.getElementById("startUpText").innerHTML = "Waiting on ignition...";
+    rpio.poll(35, rpio.PULL_DOWN);
+    rpio.poll(pin, null);
+}
+
+//after igintion the bike is just waiting to stop
+function pollIGN(pin) {
+    document.getElementById("startUpText").innerHTML = "Press the start button.";
+    rpio.poll(pin, null);
+}
+*/
