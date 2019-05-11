c73363204911d60c667822c0351f7a0668ace610
jcsmeth <jcsmeth@vt.edu>
2019-05-11 14:58:08 -0400

    console log

--- scripts.js
+++ scripts.js
@@ -439,7 +439,7 @@ function updateFault(runLO, runHI, postLO, postHI) {
   });
   var highestError = analyzeFaultSet(faultSet);
   changeFaultNum(highestError[1]);
-  //console.log("Printing now..." + highestError[0]);
+  console.log("Printing now..." + highestError[0]);
   debugError.textContent = 'Error: ' + highestError[0];
   /*
   //test output
